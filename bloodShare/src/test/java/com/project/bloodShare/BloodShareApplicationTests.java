65b1db7421ba4462aeebcaa3843e6ad4fa9c1a09
Bushra1999381 <91436455+Bushra1999381@users.noreply.github.com>
2023-07-07 21:31:55 +0300

    init commit

---
+++ bloodShare/src/test/java/com/project/bloodShare/BloodShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bloodShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/project/bloodShare/BloodShareApplication.java
@@ -0,0 +1,13 @@
+package com.project.bloodShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodShareApplication.class, args);
+	}
+
+}
